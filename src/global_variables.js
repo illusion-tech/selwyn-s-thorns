c96d46df438ed36876b19c7d2cdc2361783fd148
Wang Zishi <wangzishi@illusiontech.cn>
2023-03-24 04:34:21 +0800

    功能：显示对话选项面板

--- src/global_variables.js
+++ src/global_variables.js
@@ -2,10 +2,10 @@ const that = globalThis;
 if (that.studio_s === undefined) that.studio_s = {};
 if (that.studio_s.container === undefined) that.studio_s.container = new Map();
 if (that.studio_s.TOKEN === undefined)
-  that.studio_s.TOKEN = {
-    NECKLACE_MENU: Symbol("necklace_menu"),
-    对话选项管理器: Symbol("对话选项管理器"),
-  };
+	that.studio_s.TOKEN = {
+		NECKLACE_MENU: Symbol("necklace_menu"),
+		对话选项管理器: Symbol("对话选项管理器"),
+	};
 
 const ss = that.studio_s;
 const container = ss.container;
@@ -14,8 +14,8 @@ const 记号 = TOKEN;
 const 容器 = container;
 
 console.log({
-  that,
-  ss,
-  container,
-  TOKEN,
+	that,
+	ss,
+	container,
+	TOKEN,
 });
