82b26ad77478d90a80e9357b042acd0963640d85
Wang Zishi <wangzishi@illusiontech.cn>
2023-03-24 03:45:56 +0800

    功能：增加对话选项管理器

---
+++ src/global_variables.js
@@ -0,0 +1,21 @@
+const that = globalThis;
+if (that.studio_s === undefined) that.studio_s = {};
+if (that.studio_s.container === undefined) that.studio_s.container = new Map();
+if (that.studio_s.TOKEN === undefined)
+  that.studio_s.TOKEN = {
+    NECKLACE_MENU: Symbol("necklace_menu"),
+    对话选项管理器: Symbol("对话选项管理器"),
+  };
+
+const ss = that.studio_s;
+const container = ss.container;
+const TOKEN = ss.TOKEN;
+const 记号 = TOKEN;
+const 容器 = container;
+
+console.log({
+  that,
+  ss,
+  container,
+  TOKEN,
+});
