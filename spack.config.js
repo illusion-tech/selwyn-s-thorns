bab5b984a7d5686e409247cac0bd0305f91a18e1
Wang Zishi <wangzishi@illusiontech.cn>
2023-04-01 04:18:22 +0800

    杂项：使用 @swc/core/spack 打包

---
+++ spack.config.js
@@ -0,0 +1,10 @@
+// @ts-nocheck
+module.exports = {
+    entry: {
+        web: `${__dirname}/src/main.ts`,
+    },
+    output: {
+        path: `${__dirname}/dist`,
+        name: "bundle.js",
+    },
+};
