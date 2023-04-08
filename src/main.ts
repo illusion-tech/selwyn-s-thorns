f524ee12e6bcddfb0a8b8a2c89ddd4733a4a6143
Wang Zishi <wangzishi@illusiontech.cn>
2023-04-09 00:54:16 +0800

    功能: 项链菜单调用使用日记管理器

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,19 @@ import { CardActions } from "./用户界面/card_actions.js";
 import { 人物管理器类 } from "./用户界面/人物管理器.js";
 import { 地图管理器类 } from "./用户界面/地图管理器.js";
 import { 对话选项管理器类 } from "./用户界面/对话选项管理器.js";
+import { 日记管理器类 } from "./用户界面/日记管理器.js";
 import { 时间管理器类 } from "./用户界面/时间管理器.js";
 import { 项链菜单类 } from "./用户界面/项链菜单.js";
 
 const 人物管理器 = new 人物管理器类();
 const 对话选项管理器 = new 对话选项管理器类(人物管理器);
+
 const 时间管理器 = new 时间管理器类();
 const 地图管理器 = new 地图管理器类(时间管理器);
-const 项链菜单 = new 项链菜单类();
+
+const 日记管理器 = new 日记管理器类(时间管理器);
+const 项链菜单 = new 项链菜单类(日记管理器);
+
 const 显示项链菜单 = 项链菜单.显示.bind(项链菜单);
 
 console.log({
@@ -19,6 +24,7 @@ console.log({
     对话选项管理器,
     时间管理器,
     地图管理器,
+    日记管理器,
     项链菜单,
     显示项链菜单,
     Card,
--- src/用户界面/事件目标.ts
+++ src/用户界面/事件目标.ts
@@ -14,7 +14,7 @@ export class 事件目标 extends EventTarget {
 
 export  class 自定义事件<T> extends CustomEvent<T> {
     细节?: T;
-    constructor(类型: string, 事件参数: CustomEventInit<T> & { 细节?: T }) {
+    constructor(类型: string, 事件参数: CustomEventInit<T> & { 细节?: T } = {}) {
         事件参数.detail = 事件参数.细节;
         super(类型, 事件参数);
         this.细节 = 事件参数.细节;
