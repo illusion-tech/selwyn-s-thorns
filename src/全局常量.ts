5f65f38766f28caf29c94ca4ab955feedb17603c
Wang Zishi <wangzishi@illusiontech.cn>
2023-04-30 09:07:04 +0800

    功能：增加全局工具函数

--- src/全局常量.ts
+++ src/全局常量.ts
@@ -10,6 +10,8 @@ const 假 = false;
 const 空白 = "";
 const 未定义 = undefined;
 
+const 是否 = (真假: 真假) => (真假 ? 是 : 否);
+
 type 未定义 = typeof 未定义;
 type 空白 = typeof 空白;
 type 是 = typeof 是;
@@ -44,6 +46,7 @@ type MyGlobalThis = typeof globalThis & {
     否: 否;
     真: 真;
     假: 假;
+    是否: (真假: 真假) => 是否;
     未定义: 未定义;
     项链菜单配置: 项链菜单配置;
 };
@@ -59,5 +62,6 @@ myGlobalThis.是 = myGlobalThis.是 ?? 是;
 myGlobalThis.否 = myGlobalThis.否 ?? 否;
 myGlobalThis.真 = myGlobalThis.真 ?? 真;
 myGlobalThis.假 = myGlobalThis.假 ?? 假;
+myGlobalThis.是否 = myGlobalThis.是否 ?? 是否;
 myGlobalThis.未定义 = myGlobalThis.未定义 ?? 未定义;
 myGlobalThis.空白 = myGlobalThis.空白 ?? 空白;
