da7ffdd400fd6df5ecc7f308a4a9051336d840fc
Wang Zishi <wangzishi@illusiontech.cn>
2023-04-30 12:12:49 +0800

    重构：使用类代替对象实现人物类与属性探针类

--- src/用户界面/人物管理器.ts
+++ src/用户界面/人物管理器.ts
@@ -1,82 +1,82 @@
+import { 变量 } from "../运行时/易次元";
+
 type 属性探针检测结果 = {
-    属性: "谦逊" | "傲慢" | "荣誉";
-    变化: number;
-    旧值: number;
-    新值: number;
+    属性: 字符串;
+    变化: 数值;
+    旧值: 数值;
+    新值: 数值;
 };
 
-export class 人物管理器类 {
-    黛瑞雅 = {
-        get 谦逊() {
-            return ac.arr["黛瑞雅性格属性"][0];
-        },
-        set 谦逊(数值) {
-            ac.arr["黛瑞雅性格属性"][0] = 数值;
-        },
-        get 傲慢() {
-            return ac.arr["黛瑞雅性格属性"][1];
-        },
-        set 傲慢(数值) {
-            ac.arr["黛瑞雅性格属性"][1] = 数值;
-        },
-        get 荣誉() {
-            return ac.arr["黛瑞雅性格属性"][2];
-        },
-        set 荣誉(数值) {
-            ac.arr["黛瑞雅性格属性"][2] = 数值;
-        },
-        get 是傲慢性格() {
-            return this.傲慢 >= this.谦逊;
-        },
-        get 是强傲慢性格() {
-            return this.傲慢 - this.谦逊 >= 10;
-        },
-        get 是谦逊性格() {
-            return this.谦逊 > this.傲慢;
-        },
-        get 是强谦逊性格() {
-            return this.谦逊 - this.傲慢 >= 10;
-        },
-        get 没有强性格偏向() {
-            return Math.abs(this.傲慢 - this.谦逊) < 10;
-        },
-        获取属性探针() {
-            const that = this;
-            return {
-                谦逊: this.谦逊,
-                傲慢: this.傲慢,
-                荣誉: this.荣誉,
-                /**
-                 * 检测探针属性值的变化。
-                 */
-                检测(): 属性探针检测结果[] {
-                    const 属性探测结果 = [];
-                    if (Math.abs(that.谦逊 - this.谦逊))
-                        属性探测结果.push({
-                            属性: "谦逊",
-                            变化: that.谦逊 - this.谦逊,
-                            旧值: this.谦逊,
-                            新值: that.谦逊,
-                        } as const);
-                    if (Math.abs(that.傲慢 - this.傲慢))
-                        属性探测结果.push({
-                            属性: "傲慢",
-                            变化: that.傲慢 - this.傲慢,
-                            旧值: this.傲慢,
-                            新值: that.傲慢,
-                        } as const);
-                    if (Math.abs(that.荣誉 - this.荣誉))
-                        属性探测结果.push({
-                            属性: "荣誉",
-                            变化: that.荣誉 - this.荣誉,
-                            旧值: this.荣誉,
-                            新值: that.荣誉,
-                        } as const);
+class 属性探针类 {
+    #检测对象;
+    #旧值对象: { [属性: 字符串]: 数值 };
+    constructor(检测对象: { [属性: 字符串]: 数值 }, 属性集: 字符串[]) {
+        this.#检测对象 = 检测对象;
+        this.#旧值对象 = {};
+        for (const 属性 of 属性集) {
+            this.#旧值对象[属性] = 检测对象[属性];
+        }
+    }
 
-                    console.log({ 属性探测结果 });
-                    return 属性探测结果;
-                },
-            };
-        },
-    };
+    检测(): 属性探针检测结果[] {
+        const 属性探测结果 = [];
+
+        for (const 属性 in this.#旧值对象) {
+            const 旧值 = this.#旧值对象[属性];
+            const 新值 = this.#检测对象[属性];
+            const 变化 = 新值 - 旧值;
+            if (变化) {
+                属性探测结果.push({ 属性, 变化, 旧值, 新值 } as const);
+            }
+        }
+        console.log({ 属性探测结果 });
+        return 属性探测结果;
+    }
+}
+
+abstract class 人物类 {
+    abstract 获取属性探针(): 属性探针类;
+}
+
+class 黛瑞雅类 extends 人物类 {
+    get 谦逊() {
+        return 变量.黛瑞雅性格属性[0];
+    }
+    set 谦逊(数值) {
+        变量.黛瑞雅性格属性[0] = 数值;
+    }
+    get 傲慢() {
+        return 变量.黛瑞雅性格属性[1];
+    }
+    set 傲慢(数值) {
+        变量.黛瑞雅性格属性[1] = 数值;
+    }
+    get 荣誉() {
+        return 变量.黛瑞雅性格属性[2];
+    }
+    set 荣誉(数值) {
+        变量.黛瑞雅性格属性[2] = 数值;
+    }
+    get 是傲慢性格() {
+        return this.傲慢 >= this.谦逊;
+    }
+    get 是强傲慢性格() {
+        return this.傲慢 - this.谦逊 >= 10;
+    }
+    get 是谦逊性格() {
+        return this.谦逊 > this.傲慢;
+    }
+    get 是强谦逊性格() {
+        return this.谦逊 - this.傲慢 >= 10;
+    }
+    get 没有强性格偏向() {
+        return Math.abs(this.傲慢 - this.谦逊) < 10;
+    }
+    获取属性探针(): 属性探针类 {
+        return new 属性探针类(this as {}, ["谦逊", "傲慢", "荣誉"]);
+    }
+}
+
+export class 人物管理器类 {
+    黛瑞雅 = new 黛瑞雅类();
 }
