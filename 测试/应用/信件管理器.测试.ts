bb4f8ff0893a334152d5e492b111bd75a3255048
Wang Zishi <wangzishi@illusiontech.cn>
2023-07-23 12:53:36 +0800

    杂项：更新依赖项

--- 测试/应用/信件管理器.测试.ts
+++ 测试/应用/信件管理器.测试.ts
@@ -1,6 +1,6 @@
 import "塞尔温荆棘://测试/测试环境.ts";
 
-import { assertEquals, assertExists, assertThrows } from "https://deno.land/std@0.190.0/testing/asserts.ts";
+import { assertExists } from "https://deno.land/std@0.190.0/testing/asserts.ts";
 
 import { 信件管理器类 } from "塞尔温荆棘://src/应用/信件管理器.ts";
 
