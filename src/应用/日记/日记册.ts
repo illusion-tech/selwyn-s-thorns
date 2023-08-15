8d250cbd66f61c0dd3063361294e8a38657d5647
Wang Zishi <wangzishi@illusiontech.cn>
2023-08-16 00:37:22 +0800

    功能: 调整写日记配置

--- src/应用/日记/日记册.ts
+++ src/应用/日记/日记册.ts
@@ -1,16 +1,16 @@
-import type { 坐标, 数值, 未知, 资源标识 } from "../../运行时/全局常量.ts";
+import type { 坐标, 字符串, 数值, 未知, 资源标识 } from "../../运行时/全局常量.ts";
 import type { 日记页类 } from "./日记页.ts";
 
 export interface 成册配置 {
+    资源标识: 资源标识;
     贴纸配置: {
         资源标识: 资源标识;
         位置: 坐标;
         对应装订页: 日记页类;
     }[];
+
 }
 
 export class 日记册类 {
-    constructor(成册配置: 成册配置, ...页集: 日记页类[]) {
-        
-    }
+    constructor(成册配置: 成册配置, ...页集: 日记页类[]) {}
 }
