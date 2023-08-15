379d609757068b5332e53468cbfebac59c38aac0
Wang Zishi <wangzishi@illusiontech.cn>
2023-08-16 01:18:20 +0800

    杂项: 清理无用代码

--- src/应用/日记/日记本.ts
+++ src/应用/日记/日记本.ts
@@ -1,11 +1,5 @@
 import { 选项记录器类 } from "../../用户界面/选项记录器.ts";
-import type { 任意, 字符串, 数值, 是否 } from "../../运行时/全局常量.ts";
 import type { 日记册类 } from "./日记册.ts";
-
-interface 段落 {
-    自动书写: 是否;
-    文字内容: () => 字符串;
-}
 
 export class 日记本类 {
     #选项记录器: 选项记录器类;
