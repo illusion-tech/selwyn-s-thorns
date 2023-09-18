c0c1a32b08603cb291fea363763a2d35f2f13a49
Wang Zishi <wangzishi@illusiontech.cn>
2023-09-19 01:37:00 +0800

    杂项: 修改打包配置文件

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,6 @@
 // import { nodeResolve } from "@rollup/plugin-node-resolve";
-// import typescript from "@rollup/plugin-typescript";
-import { swc as typescript } from "rollup-plugin-swc3";
+import typescript from "@rollup/plugin-typescript";
+// import { swc as typescript } from "rollup-plugin-swc3";
 
 /**
  * @type {import('rollup').RollupOptions}
