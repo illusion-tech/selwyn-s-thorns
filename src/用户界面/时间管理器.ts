6b32b3fa18dd0358e84fa3efe8c20ba1913201a4
Wang Zishi <wangzishi@illusiontech.cn>
2023-10-27 14:36:02 +0800

    功能: 使用内存变量获取日期时间

--- src/用户界面/时间管理器.ts
+++ src/用户界面/时间管理器.ts
@@ -4,28 +4,46 @@ import { 变量 } from "../运行时/易次元.ts";
 export type 月份 = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 
 export class 时间管理器类 {
+    get #日期时间() {
+        // 1_1991_0605_1200;
+
+        const 数值 = 变量.内存[999];
+        if (数值 < 1_0000_0000_000) throw new Error(`内存 #999 的值 ${数值} 过小不是一个有效的日期时间数值`);
+        if (数值 > 1_9999_1231_2359) throw new Error(`内存 #999 的值 ${数值} 过大不是一个有效的日期时间数值`);
+
+        const 日期字符串 = 数值.toString().substring(1);
+        const 结果 = [] as 数值[];
+
+        结果.push(parseInt(日期字符串.substring(0, 4))); // 年
+        结果.push(parseInt(日期字符串.substring(4, 6))); // 月
+        结果.push(parseInt(日期字符串.substring(6, 8))); // 日
+        结果.push(parseInt(日期字符串.substring(8, 10))); // 时
+        结果.push(parseInt(日期字符串.substring(10, 12))); // 分
+
+        return 结果 as [年: 数值, 月: 数值, 日: 数值, 时: 数值, 分: 数值];
+    }
     get 现在是上午() {
-        return 变量.日期时间[3] < 12;
+        return this.#日期时间[3] < 12;
     }
 
     get 现在是下午() {
-        return 变量.日期时间[3] >= 12 && 变量.日期时间[3] < 18;
+        return this.#日期时间[3] >= 12 && this.#日期时间[3] < 18;
     }
 
     get 现在是晚上() {
-        return 变量.日期时间[3] >= 18;
+        return this.#日期时间[3] >= 18;
     }
 
     get 现在是凌晨() {
-        return 变量.日期时间[3] < 6;
+        return this.#日期时间[3] < 6;
     }
 
     get 现在是白天() {
-        return 变量.日期时间[3] >= 6 && 变量.日期时间[3] < 18;
+        return this.#日期时间[3] >= 6 && this.#日期时间[3] < 18;
     }
 
     get 现在是黑夜() {
-        return 变量.日期时间[3] < 6 || 变量.日期时间[3] >= 18;
+        return this.#日期时间[3] < 6 || this.#日期时间[3] >= 18;
     }
 
     设置日期(年: 数值, 月: 数值, 日: 数值) {
@@ -40,15 +58,15 @@ export class 时间管理器类 {
     }
 
     获取当前时间() {
-        return 变量.日期时间;
+        return this.#日期时间;
     }
 
     获取当前年份(): 数值 {
-        return 变量.日期时间[0];
+        return this.#日期时间[0];
     }
 
     获取当前月份(): 月份 {
-        const 月份 = 变量.日期时间[1];
+        const 月份 = this.#日期时间[1];
         if (月份 < 1) return 1;
         if (月份 > 12) return 12;
         return 月份 as 月份;
@@ -58,9 +76,9 @@ export class 时间管理器类 {
      * @备注 返回的示例字符串 '1991-06-05'
      */
     获取当前日期字符串() {
-        const 年 = 变量.日期时间[0].toString().padStart(4, "0");
-        const 月 = 变量.日期时间[1].toString().padStart(2, "0");
-        const 日 = 变量.日期时间[2].toString().padStart(2, "0");
+        const 年 = this.#日期时间[0].toString().padStart(4, "0");
+        const 月 = this.#日期时间[1].toString().padStart(2, "0");
+        const 日 = this.#日期时间[2].toString().padStart(2, "0");
         return `${年}-${月}-${日}` as `${数值}-${数值}-${数值}`;
     }
 }
