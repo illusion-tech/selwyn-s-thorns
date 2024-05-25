b60be98e5577210a0e47fd57928db3c7536e8249
Wang Zishi <wangzishi@illusiontech.cn>
2024-05-26 02:49:59 +0800

    杂项: 增加 Temporal API 垫片

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,14 +1,14 @@
-// import { nodeResolve } from "@rollup/plugin-node-resolve";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 
 /**
  * @type {import('rollup').RollupOptions}
  */
 const config = {
-    input: "src/main.ts",
-    plugins: [typescript() /*, nodeResolve() */],
-    output: {
-        file: "dist/bundle.rollup.js",
-    },
+  input: "src/main.ts",
+  plugins: [typescript(), nodeResolve()],
+  output: {
+    file: "dist/bundle.rollup.js",
+  },
 };
 export default config;
